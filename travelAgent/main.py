ce2f000c3178637667bf7af4f06484bb67c7080f
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-10-30 00:06:12 +0800

    Initial playground

---
+++ travelAgent/main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import transformers
+import torch
+
+load_dotenv()
+
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+    token=token,
+)
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a pirate chatbot who always responds in pirate speak!",
+    },
+    {"role": "user", "content": "Who are you?"},
+]
+
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
