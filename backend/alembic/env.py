02ead78d57174d9d61f925ef8346d33ee47ab36f
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-12-04 15:56:22 +0800

    create user api

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,7 +11,7 @@ load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", os.getenv("ALEMBIC_CONNECTION_STRING"))
+config.set_main_option("sqlalchemy.url", os.getenv("POSTGRES_CONNECTION_STRING"))
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- backend/src/services/userService.py
+++ backend/src/services/userService.py
@@ -12,15 +12,15 @@ class UserService:
         logger.info("UserService initialized")
 
     async def create_user(self, user: UserRequest, db: Session):
-        existUser = await db.query(User).filter_by(email=user.email).first()
+        existUser = db.query(User).filter_by(email=user.email).first()
 
         if existUser:
             logger.error("User account already exists!")
             return None
 
-        newUser = User(user.username, user.email)
-        await db.add(newUser)
-        await db.commit()
+        newUser = User(username=user.username, email=user.email)
+        db.add(newUser)
+        db.commit()
         logger.info(f"Create new user {newUser.id} successfully")
         return CreateUserResponse(
             id=newUser.id, email=newUser.email, username=newUser.username
