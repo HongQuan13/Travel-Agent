49c3cfbbf8b7946582ab573cc84d143872f11b41
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-12-12 12:40:01 +0800

    duckduckgo search tool

--- travel_agent/agent_handler/agent_manager.py
+++ travel_agent/agent_handler/agent_manager.py
@@ -3,11 +3,11 @@ import logging
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.prebuilt import create_react_agent, chat_agent_executor
+from langgraph.prebuilt import create_react_agent
 from langchain_ollama import ChatOllama
 
 from travel_agent.helpers.agent_constant import PROMPT_TEMPLATE
-from travel_agent.helpers.agent_tools.agent_tools import google_search
+from travel_agent.helpers.agent_tools.text_search import google_search
 
 logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger(__name__)
---
+++ travel_agent/helpers/agent_tools/text_search.py
@@ -0,0 +1,18 @@
+import os
+from langchain.tools import tool
+from langchain_community.utilities.google_serper import GoogleSerperAPIWrapper
+from langchain_community.tools import DuckDuckGoSearchRun
+
+
+@tool()
+def google_search(query: str) -> str:
+    """Use when searching information from Internet"""
+    serper = GoogleSerperAPIWrapper(serper_api_key=os.getenv("SERPER_API_KEY"))
+    return serper.run(query)
+
+
+@tool()
+def duckduckgo_search(query: str) -> str:
+    """Use when searching information from Internet"""
+    search = DuckDuckGoSearchRun()
+    return search.run(query)
