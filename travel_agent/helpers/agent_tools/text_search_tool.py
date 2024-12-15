d9f8c635a1b2199abed5e3aafae0938355aaac1a
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-12-15 16:50:19 +0800

    change text_search description

--- travel_agent/helpers/agent_tools/text_search_tool.py
+++ travel_agent/helpers/agent_tools/text_search_tool.py
@@ -6,13 +6,13 @@ from langchain_community.utilities.google_serper import GoogleSerperAPIWrapper
 
 @tool()
 def google_search(query: str) -> str:
-    """Use when searching information from Internet"""
+    """Use when searching information about a place or event. Do not use it for image search"""
     serper = GoogleSerperAPIWrapper(serper_api_key=os.getenv("SERPER_API_KEY"))
     return serper.run(query)
 
 
 @tool()
 def duckduckgo_search(query: str) -> str:
-    """Use when searching information from Internet"""
+    """Use when searching information about a place or event. Do not use it for image search"""
     search = DuckDuckGoSearchRun()
     return search.run(query)
