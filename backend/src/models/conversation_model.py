40b413b01cb89414ddad06890d472b15d005eb83
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-12-18 15:54:55 +0800

    user, conversaiton one-to-many relation

--- backend/src/models/conversation_model.py
+++ backend/src/models/conversation_model.py
@@ -1,7 +1,11 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import Integer, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.src.models.base_model import Base
+
+if TYPE_CHECKING:
+    from backend.src.models.user_model import User
 
 
 class Conversation(Base):
@@ -9,3 +13,5 @@ class Conversation(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="conversation")
--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -1,8 +1,9 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, relationship
 
 from backend.src.models.base_model import Base
+from backend.src.models.conversation_model import Conversation
 
 
 class User(Base):
@@ -10,6 +11,8 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(30), nullable=False)
     email: Mapped[str] = mapped_column(unique=True)
+
+    conversation: Mapped["Conversation"] = relationship(back_populates="user")
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, username={self.username!r}, email={self.email!r})"
