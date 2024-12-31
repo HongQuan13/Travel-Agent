c22d26ce7d527564e63d2d4fc0fc699e8c2806fa
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-12-31 16:58:41 +0800

    solve tool message error

--- travel_agent/helpers/postgres_checkpoint.py
+++ travel_agent/helpers/postgres_checkpoint.py
@@ -1,4 +1,5 @@
 from langgraph.checkpoint.postgres import PostgresSaver
+from langchain_core.messages import ToolMessage
 
 
 class PostgresSaverWithLimit(PostgresSaver):
@@ -20,36 +21,31 @@ class PostgresSaverWithLimit(PostgresSaver):
         for checkpoint in checkpoints:
             if "messages" in checkpoint.checkpoint["channel_values"]:
                 checkpoint.checkpoint["channel_values"]["messages"] = (
-                    checkpoint.checkpoint["channel_values"]["messages"][
-                        -self.limit_message :
-                    ]
+                    self._handle_message_limit(
+                        checkpoint.checkpoint["channel_values"]["messages"]
+                    )
                 )
             yield checkpoint
-
-    def put(self, config, checkpoint, metadata, new_versions):
-        channel_values = checkpoint.get("channel_values", {})
-
-        if "messages" in channel_values:
-            channel_values["messages"] = channel_values["messages"][
-                -self.limit_message :
-            ]
-
-        checkpoint["channel_values"] = channel_values
-
-        return super().put(config, checkpoint, metadata, new_versions)
 
     def get_tuple(self, config):
         """Get a checkpoint tuple from the database, limiting channel values to the latest 10 messages."""
 
-        # Call the parent class's get_tuple method to retrieve the checkpoint
         checkpoint_tuple = super().get_tuple(config)
 
         if checkpoint_tuple:
             if "messages" in checkpoint_tuple.checkpoint["channel_values"]:
                 checkpoint_tuple.checkpoint["channel_values"]["messages"] = (
-                    checkpoint_tuple.checkpoint["channel_values"]["messages"][
-                        -self.limit_message :
-                    ]
+                    self._handle_message_limit(
+                        checkpoint_tuple.checkpoint["channel_values"]["messages"]
+                    )
                 )
 
         return checkpoint_tuple
+
+    def _handle_message_limit(self, checkpoint_message):
+        first_message = checkpoint_message[-self.limit_message]
+
+        if isinstance(first_message, ToolMessage):
+            self.limit_message += 1
+
+        return checkpoint_message[-self.limit_message :]
