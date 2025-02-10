6b2e6f3984f19aa0228de57b5f2f07f8f9b06bd9
Quân Nguyễn Hồng <nguyenhongquan13082003@gmail.com>
2025-02-10 15:08:59 +0800

    using supabase db

--- backend/src/dbs/init_postgres.py
+++ backend/src/dbs/init_postgres.py
@@ -25,7 +25,8 @@ class Database:
         self._create_connection()
 
     def _check_env(self) -> None:
-        connection_string = os.getenv("DB_CONNECTION_STRING")
+        connection_string = os.getenv("SUPABASE_DB_CONNECTION_STRING")
+
         if connection_string == None:
             raise ValueError(
                 f"Unable to access database connection string in .env file"
